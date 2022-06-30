a541695ab5fd412d7c750266701c3a504109bdc9
emanueledb <emanueledibene@gmail.com>
2022-06-30 19:18:34 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,11 @@ import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
+  name: "Home",
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+}
 </script>
