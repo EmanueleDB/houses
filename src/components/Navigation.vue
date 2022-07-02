1ee70e5f4ac61bcc6e3770188ffca866c356592c
emanueledb <emanueledibene@gmail.com>
2022-07-02 12:46:07 +0200

    setup navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navigation">
+    <div class="container">
+      <img src="../static/images/img_logo_dtt@3x.png" alt="logo" />
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss">
+.container {
+  display: flex;
+
+}
+</style>
