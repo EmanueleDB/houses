b65e1b913f2ee747ba11e0de9624a90b447ffb59
emanueledb <emanueledibene@gmail.com>
2022-07-02 13:59:07 +0200

    menu items styled

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,62 @@
 <template>
   <nav class="navigation">
-    <div class="container">
-      <img src="../static/images/img_logo_dtt@3x.png" alt="logo" />
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="navigation__container">
+      <img
+        class="navigation__container__image"
+        src="../static/images/img_logo_dtt@3x.png"
+        alt="logo"
+      />
+      <ul v-for="item in navItems" :key="item.id">
+        <router-link :class="['navigation__container__item']" :to="item.url">{{
+          item.name
+        }}</router-link>
+      </ul>
     </div>
   </nav>
 </template>
 
-<style lang="scss">
-.container {
-  display: flex;
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Navigation",
+  data() {
+    return {
+      navItems: [
+        { id: 1, url: "/", name: "Houses" },
+        { id: 2, url: "/about", name: "About" },
+      ],
+    };
+  },
+};
+</script>
 
+<style lang="scss">
+.navigation {
+  display: flex;
+  height: 45px;
+
+  &__container {
+    display: flex;
+    align-items: center;
+    margin: auto;
+
+    &__image {
+      height: 45px;
+      margin-right: 30px;
+    }
+
+    &__item {
+      color: $tertiary;
+      @include font-montserrat-regular;
+      font-size: 18px;
+      text-decoration: unset;
+      padding: 10px;
+      margin-right: 30px;
+    }
+    .router-link-exact-active {
+      @include font-montserrat-bold;
+      color: $text-primary;
+    }
+  }
 }
 </style>
