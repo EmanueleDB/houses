079dbaaa53f3100ffac9c7344c141754118d2608
emanueledb <emanueledibene@gmail.com>
2022-07-03 18:19:39 +0200

    sort implemented

--- src/components/Home/Houses/Listings.vue
+++ src/components/Home/Houses/Listings.vue
@@ -73,6 +73,9 @@ export default {
   },
   computed: {
     availableHouses() {
+      if (this.filter === "price")
+        this.houses = this.houses.sort((a, b) => (a.price > b.price ? 1 : -1))
+      else this.houses = this.houses.sort((a, b) => (a.size > b.size ? 1 : -1))
       if (this.searchQuery === "") return this.houses
       return this.houses?.filter((house) =>
         house.location.street?.includes(this.searchQuery)
