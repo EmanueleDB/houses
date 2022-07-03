b661791facbc7bca2e75d4b82b0703a5289bca54
emanueledb <emanueledibene@gmail.com>
2022-07-03 13:18:44 +0200

    eslint fixed - house list wip

--- src/components/Houses.vue
+++ src/components/Houses.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="houses">
+  <div>
     <div class="container">
       <div class="container__row">
         <h1>Houses</h1>
         <div class="container__row__button-container">
-          <button class="container__row__button-container__button">
+          <button
+            v-if="displaySize"
+            class="container__row__button-container__button"
+          >
             <img
               class="container__row__button-container__button__icon"
               src="../static/images/ic_plus_white@3x.png"
@@ -46,16 +49,62 @@
           </button>
         </div>
       </div>
-      <div class="container__list">
-        <h1>sds</h1>
-        <button>dsfdsfdsf</button>
+      <div class="container__house-list">
+        <div
+          v-for="house in houses"
+          class="container__house-list__house"
+          :key="house.id"
+        >
+          <img
+            :src="house.image"
+            alt="house-image"
+            class="container__house-list__house__image"
+          />
+          <div class="container__house-list__house__details">
+            <p class="container__house-list__house__details__street">
+              {{ house.location.street }}
+            </p>
+            <p class="container__house-list__house__details__price">
+              &euro; {{ house.price }}
+            </p>
+            <p class="container__house-list__house__details__zip">
+              {{ house.location.zip }} {{ house.location.city }}
+            </p>
+            <div class="container__house-list__house__details__rooms">
+              <img
+                class="container__house-list__house__details__rooms__image"
+                src="../static/images/ic_bed@3x.png"
+                alt="bed"
+              />
+              <p class="container__house-list__house__details__rooms__specs">
+                {{ house.rooms.bedrooms }}
+              </p>
+              <img
+                class="container__house-list__house__details__rooms__image"
+                src="../static/images/ic_bath@3x.png"
+                alt="baths"
+              />
+              <p class="container__house-list__house__details__rooms__specs">
+                {{ house.rooms.bathrooms }}
+              </p>
+              <img
+                class="container__house-list__house__details__rooms__image"
+                src="../static/images/ic_size@3x.png"
+                alt="size"
+              />
+              <p class="container__house-list__house__details__rooms__specs">
+                {{ house.size }}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -67,105 +116,173 @@ export default {
     return {
       clicked: "price",
       houses: [],
-    };
+      displaySize: null,
+    }
+  },
+  computed: {},
+  watch: {
+    screenSize(to) {
+      console.log(to)
+      if (to > 450) this.displaySize = true
+      this.displaySize = false
+    },
   },
   mounted() {
-    this.getHouses();
+    window.addEventListener("resize", this.screenSize)
+
+    this.getHouses()
   },
   methods: {
     async getHouses() {
       try {
-        const headers = { "X-Api-Key": process.env.VUE_APP_APIKEY };
+        const headers = { "X-Api-Key": process.env.VUE_APP_APIKEY }
         const response = await axios.get(
           "https://api.intern.d-tt.nl/api/houses",
           { headers }
-        );
-        this.houses = response.data;
-        console.log(response.data);
+        )
+        this.houses = response.data
+        console.log(response.data)
       } catch (e) {
-        console.log(e);
+        console.log(e)
       }
     },
+    screenSize(e) {
+      this.displaySize = e.target
+      console.log(e.innerWidth)
+    },
   },
-};
+}
 </script>
 
 <style lang="scss">
-.houses {
-  .container {
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
+.container {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
 
-    &__row,
-    &__list {
-      //@include respond-to("md") {
-      //  background-color: orange;
-      //}
-      position: relative;
-      display: flex;
-      justify-content: space-between;
-      grid-column: span 12;
-      margin-bottom: 2rem;
+  &__row {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    grid-column: span 12;
+    margin-bottom: 2rem;
 
-      &__button-container {
-        margin: auto 0;
-        &__button {
-          @include font-montserrat-semiBold;
-          width: 150px;
-          padding: 10px;
-          border-radius: 5px;
-          border: unset;
-          text-transform: uppercase;
-          background-color: $primary;
-          color: $white;
-          cursor: pointer;
-          &__icon {
-            width: 11px;
-            margin-right: 5px;
-          }
+    &__button-container {
+      margin: auto 0;
+      &__button {
+        @include font-montserrat-semiBold;
+        width: 150px;
+        padding: 10px;
+        border-radius: 5px;
+        border: unset;
+        text-transform: uppercase;
+        background-color: $primary;
+        color: $white;
+        cursor: pointer;
+        &__icon {
+          width: 11px;
+          margin-right: 5px;
         }
       }
+    }
 
-      &__search-icon {
-        position: absolute;
-        width: 15px;
-        height: 15px;
-        top: 50%;
-        left: 20px;
-        transform: translate(-50%, -50%);
+    &__search-icon {
+      position: absolute;
+      width: 15px;
+      height: 15px;
+      top: 50%;
+      left: 20px;
+      transform: translate(-50%, -50%);
+      color: $tertiary;
+    }
+
+    &__input-text {
+      width: 40%;
+      border: unset;
+      border-radius: 5px;
+      padding: 10px 0 10px 40px;
+      background-color: $tertiary-light;
+      &::placeholder {
         color: $tertiary;
       }
+    }
 
-      &__input-text {
-        width: 40%;
+    &__tab {
+      &__price,
+      &__size {
+        background-color: $tertiary;
+        width: 110px;
+        color: $white;
+        padding: 10px 0;
         border: unset;
-        border-radius: 5px;
-        padding: 10px 0 10px 40px;
-        background-color: $tertiary-light;
-        &::placeholder {
-          color: $tertiary;
+        cursor: pointer;
+
+        &.active {
+          background-color: $primary;
         }
       }
+      &__price {
+        border-radius: 5px 0 0 5px;
+      }
+      &__size {
+        border-radius: 0 5px 5px 0;
+      }
+    }
+  }
 
-      &__tab {
+  &__house-list {
+    display: flex;
+    flex-direction: column;
+    grid-column: span 12;
+    justify-content: space-between;
+    width: 60%;
+
+    &__house {
+      display: flex;
+      flex-direction: row;
+      margin-bottom: 3rem;
+
+      &__image {
+        width: 150px;
+        border-radius: 5px;
+        margin-right: 2rem;
+
+        @include respond-to("xs") {
+          margin-right: 1rem;
+        }
+      }
+      &__details {
+        padding: 10px;
+
+        &__street {
+          @include font-montserrat-bold;
+          font-size: 16px;
+          margin-bottom: 5px;
+        }
         &__price,
-        &__size {
-          background-color: $tertiary;
-          width: 110px;
-          color: $white;
-          padding: 10px 0;
-          border: unset;
-          cursor: pointer;
+        &__zip {
+          @include font-montserrat-regular;
+          font-size: 14px;
+          margin-bottom: 5px;
+        }
+        &__zip {
+          color: $tertiary;
+        }
+        &__rooms {
+          display: flex;
+          align-items: center;
 
-          &.active {
-            background-color: $primary;
+          &__image {
+            width: 15px;
+            height: 15px;
+            margin-right: 10px;
           }
-        }
-        &__price {
-          border-radius: 5px 0 0 5px;
-        }
-        &__size {
-          border-radius: 0 5px 5px 0;
+
+          &__specs {
+            @include font-montserrat-regular;
+            font-size: 14px;
+            color: $tertiary;
+            margin-right: 10px;
+          }
         }
       }
     }
