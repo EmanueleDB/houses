43f175cdb67a7bd3fe0a37e10e8be1592f308d63
emanueledb <emanueledibene@gmail.com>
2022-07-04 16:00:01 +0200

    WIP

--- src/views/ListingDetails/ListingDetails.vue
+++ src/views/ListingDetails/ListingDetails.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="container">
-    <div class="container__top">
-      <div class="container__top__back">
+    <div class="container__detail">
+      <div class="container__detail__back">
         <img
-          class="container__top__back__icon"
+          class="container__detail__back__icon"
           src="../../static/images/ic_back_grey@3x.png"
           alt="back"
-          @click="$router.push({path: '/' })"
+          @click="$router.push({ path: '/' })"
         />
         <p>Back to overview</p>
       </div>
     </div>
-    <div class="container__top">
-      <img :src="listing.image" alt="image" />
+    <div class="container__detail">
+      <img class="container__detail__image" :src="listing.image" alt="image" />
+      <div class="container__detail__reccomended">
+        <h3>Reccomended for you</h3>
+        <div class="container__detail__reccomended__wrapper">
+          <div v-for="house in getHouses" :key="house.id">
+            <div class="container__detail__reccomended__wrapper__specs" v-if="house.id !== listing.id && house.id < 6">
+              <img
+                class="container__detail__reccomended__wrapper__specs__image"
+                :src="house.image"
+                alt="house-image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -32,6 +46,7 @@ export default {
   },
   mounted() {
     this.findListing()
+    console.log(this.getHouses)
   },
   methods: {
     findListing() {
